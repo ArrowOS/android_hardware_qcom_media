94bae8351cced2d6c32e92295f2600eac38cd5d0
Pig <pig.priv@gmail.com>
2020-09-19 09:24:07 +0800

    media: Drop LOCAL_COPY_HEADERS usage
    LOCAL_COPY_HEADERS is deprecated, so remove all its usages
    to avoid build warnings.
    
    Change-Id: I8c9de08610e8d94da568b46b3e8c221007c850a0

--- libplatformconfig/Android.mk
+++ libplatformconfig/Android.mk
@@ -4,8 +4,6 @@ LOCAL_PATH := $(call my-dir)
 LOCAL_DIR_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_COPY_HEADERS_TO := libplatformconfig
-
 libplatformconfig-def := \
       -g0 -O3 -fpic \
       -Wno-deprecated-declarations -Werror \
@@ -16,8 +14,6 @@ libplatformconfig-def := \
     -D_ANDROID_
 
 COMMON_CFLAGS := -O3
-
-include $(BUILD_COPY_HEADERS)
 
 LOCAL_CFLAGS := $(COMMON_CFLAGS) $(libplatformconfig-def)
 
